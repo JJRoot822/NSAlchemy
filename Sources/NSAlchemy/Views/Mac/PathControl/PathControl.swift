079014f733447c5ef43778cc72076d85f53361ed
JJRoot822 <josh.root822@gmail.com>
2025-01-28 10:39:38 -0600

    Removed the double-click action from PathControl

--- Sources/NSAlchemy/Views/Mac/PathControl/PathControl.swift
+++ Sources/NSAlchemy/Views/Mac/PathControl/PathControl.swift
@@ -11,28 +11,20 @@ import SwiftUI
 @available(macOS 10.15, *)
 public struct PathControl: NSViewRepresentable {
     var url: URL
-    var doubleClickAction: (() -> Void)?
     
     /// Creates a PathControl with a URL to display and an optional double click action.
     /// - Parameters:
     ///   - url: The URL object that represents the path to a file on the user's system.
-    ///   - doubleClickAction: An optional double click handler.
-    public init(url: URL, doubleClickAction: (() -> Void)? = nil) {
+    public init(url: URL) {
         self.url = url
-        self.doubleClickAction = doubleClickAction
     }
     
     public func makeNSView(context: Context) -> NSPathControl {
         let pathControl = NSPathControl()
         pathControl.url = url
-        pathControl.doubleAction = #selector(context.coordinator.doubleClicked)
         
         return pathControl
     }
     
     public func updateNSView(_ nsView: NSPathControl, context: Context) {}
-    
-    public func makeCoordinator() -> Coordinator {
-        return Coordinator(self)
-    }
 }
